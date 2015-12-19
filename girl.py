e33cd6f821829c26eb97587be28886db9e6025b8
ljc <ljcnot@126.com>
2015-12-19 17:33:02 +0800

    增加girl的线程处理，但是还有问题 尚在解决

--- girl.py
+++ girl.py
@@ -2,12 +2,21 @@ import os, sys, requests, uuid, time
 import urllib.request
 from bs4 import BeautifulSoup
 import socket
+import threading
 socket.setdefaulttimeout(5.0)
+p_jclock = threading.Condition() #进程锁调用
+p_allPath = [] #所有解析的url链接表
+p_stanPath = [] #等待爬取的url链接表
+p_existPath = [] #已经爬取过的url链接表
+p_errorPath = [] #下载失败的url链接表
+p_downNum = 0 #已经下载过的连接数
+p_threadNum = 10
+p_threadpool = []
 
 def generateFileName(name):
     return str(uuid.uuid3(uuid.NAMESPACE_URL,name))
 
-      #mybaby
+
 # 根据文件名创建文件
 def createFileWithFileName(localPathParam,fileName):
     totalPath=localPathParam+'\\'+fileName
@@ -17,7 +26,7 @@ def createFileWithFileName(localPathParam,fileName):
         return totalPath
 
 def meiziImg(url):
-    s = requests.get(url,data=data,headers=headers)
+    s = requests.get(url,data=data,headers=headers_value())
     bs = BeautifulSoup(s.text,"html.parser")
     girl=bs.find_all('div',class_="postContent")
     for child in girl:
@@ -39,7 +48,7 @@ def meiziImg(url):
     return false
 
 def qzurl(url):
-    s = requests.get(url,data=data,headers=headers)
+    s = requests.get(url,data=data,headers=headers_value())
     bs = BeautifulSoup(s.text,"html.parser")
     t="http://www.meizitu.com/a/sifang_5_1.html"
     g = bs.find_all('li', class_="wp-item")
@@ -48,14 +57,24 @@ def qzurl(url):
             l=m.get("href")
             if t!=l:
                 print(l)
-                j=meiziImg(l)
+                j= threading.Thread(target=meiziImg, args=l)
+                j.setDaemon(True)
+                j.start()
+                p_threadpool.append(j)
                 t=l
-                if j:
-                    continue
+    for i in p_threadpool:
+        i.join(30)
+
+
     return false
+myReferer = ""
+def headers_value():
+    headers = {"Host": "www.meizitu.com","User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:43.0) Gecko/20100101 Firefox/43.0","Accept": "image/png,image/*;q=0.8,*/*;q=0.5","Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3","Accept-Encoding": "gzip, deflate","Referer":myReferer,"Cookie": "BAIDUID=E7640F5E0492BC620C23C0A8B7012A1C:FG=1; HMACCOUNT=5DA7BAB08C224046; BIDUPSID=E7640F5E0492BC620C23C0A8B7012A1C; PSTM=1450504738; H_PS_PSSID=17747_1420_18240_12824_18501_17001_17073_15742_12089","Connection": "keep-alive"}
+    return headers
 def fanye(url):
     g = r"http://www.meizitu.com/a/"
-    s = requests.get(url, data=data, headers=headers)
+    myReferer = url
+    s = requests.get(url, data=data, headers=headers_value())
     gs = BeautifulSoup(s.text,"html.parser")
     i = gs.find_all(id = "wp_page_numbers")
     for c in i:
@@ -66,9 +85,13 @@ def fanye(url):
             j = qzurl(u)
             if j:
                continue
+            p_threadpool = []
     return false
+
+
 def mulu(url):
-    s= requests.get(url, data=data, headers=headers)
+    myReferer = url
+    s= requests.get(url, data=data, headers=headers_value())
     gs= BeautifulSoup(s.text, "html.parser")
     i = gs.find_all("span")
     for c in i:
@@ -85,10 +108,9 @@ papap=targetDir+"\\"
 
 url2 = "http://www.meizitu.com/a/sifang_5_1.html"
 data=None
-headers = {
-    'content-type': 'application/json',
-           'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100101 Firefox/22.0'
-    }
+
+
+
 false = False
 try:
     
---
+++ 多线程代码.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+#coding=utf-8
+import threading
+import urllib
+import re
+import time
+
+g_mutex=threading.Condition()
+g_pages=[]  #从中解析所有url链接
+g_queueURL=[]  #等待爬取的url链接列表
+g_existURL=[]  #已经爬取过的url链接列表
+g_failedURL=[] #下载失败的url链接列表
+g_totalcount=0 #下载过的页面数
+
+class Crawler:
+    def __init__(self,crawlername,url,threadnum):
+        self.crawlername=crawlername
+        self.url=url
+        self.threadnum=threadnum
+        self.threadpool=[]
+        self.logfile=file("log.txt",'w')
+    def craw(self):
+        global g_queueURL
+        g_queueURL.append(url)
+        depth=0
+        print self.crawlername+" 启动..."
+        while(len(g_queueURL)!=0):
+            depth+=1
+            print 'Searching depth ',depth,'...\n\n'
+            self.logfile.write("URL:"+g_queueURL[0]+"........")
+            self.downloadAll()
+            self.updateQueueURL()
+            content='\n>>>Depth '+str(depth)+':\n'
+            self.logfile.write(content)
+            i=0
+            while i<len(g_queueURL):
+                content=str(g_totalcount+i)+'->'+g_queueURL[i]+'\n'
+                self.logfile.write(content)
+                i+=1
+    def downloadAll(self):
+        global g_queueURL
+        global g_totalcount
+        i=0
+        while i<len(g_queueURL):
+            j=0
+            while j<self.threadnum and i+j < len(g_queueURL):
+                g_totalcount+=1
+                threadresult=self.download(g_queueURL[i+j],str(g_totalcount)+'.html',j)
+                if threadresult!=None:
+                    print 'Thread started:',i+j,'--File number =',g_totalcount
+                j+=1
+            i+=j
+            for thread in self.threadpool:
+                thread.join(30)
+            threadpool=[]
+        g_queueURL=[]
+    def download(self,url,filename,tid):
+        crawthread=CrawlerThread(url,filename,tid)
+        self.threadpool.append(crawthread)
+        crawthread.start()
+    def updateQueueURL(self):
+        global g_queueURL
+        global g_existURL
+        newUrlList=[]
+        for content in g_pages:
+            newUrlList+=self.getUrl(content)
+        g_queueURL=list(set(newUrlList)-set(g_existURL))
+    def getUrl(self,content):
+        reg=r'"(http://.+?)"'
+        regob=re.compile(reg,re.DOTALL)
+        urllist=regob.findall(content)
+        return urllist
+
+class CrawlerThread(threading.Thread):
+    def __init__(self,url,filename,tid):
+        threading.Thread.__init__(self)
+        self.url=url
+        self.filename=filename
+        self.tid=tid
+    def run(self):
+        global g_mutex
+        global g_failedURL
+        global g_queueURL
+        try:
+            page=urllib.urlopen(self.url)
+            html=page.read()
+            fout=file(self.filename,'w')
+            fout.write(html)
+            fout.close()
+        except Exception,e:
+            g_mutex.acquire()
+            g_existURL.append(self.url)
+            g_failedURL.append(self.url)
+            g_mutex.release()
+            print 'Failed downloading and saving',self.url
+            print e
+            return None
+        g_mutex.acquire()
+        g_pages.append(html)
+        g_existURL.append(self.url)
+        g_mutex.release()
+
+
+if __name__=="__main__":
+    url=raw_input("请输入url入口:\n")
+    threadnum=int(raw_input("设置线程数:"))
+    crawlername="小小爬虫"
+    crawler=Crawler(crawlername,url,threadnum)
+    crawler.craw()
